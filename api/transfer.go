293c46062ea7623f88fcb689c61b5a1128861dca
Ryu <juker1141@gmail.com>
2023-06-05 18:01:29 +0800

    update transfer.go

--- api/transfer.go
+++ api/transfer.go
@@ -1,29 +1,68 @@
 package api
 
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/juker1141/simplebank/db/sqlc"
+)
+
 type transferRequest struct {
-	FromAccountID     int64 `json:"from_account_id" binding:"required,min=1"`
-	ToAccountID    		int64 `json:"to_account_id" binding:"required,min=1"`
+	FromAccountID     int64  `json:"from_account_id" binding:"required,min=1"`
+	ToAccountID    		int64  `json:"to_account_id" binding:"required,min=1"`
+	Amount						int64  `json:"amount" binding:"required,gt=0"`
 	Currency 					string `json:"currency" binding:"required,oneof=USD EUR"`
 }
 
-// func (server *Server) createAccount(ctx *gin.Context) {
-// 	var req createAccountRequest
-// 	if err := ctx.ShouldBindJSON(&req); err != nil {
-// 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-// 		return
-// 	}
+func (server *Server) createTransfer(ctx *gin.Context) {
+	var req transferRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
 
-// 	arg := db.CreateAccountParams{
-// 		Owner: req.Owner,
-// 		Currency: req.Currency,
-// 		Balance: 0,
-// 	}
+	if !server.validAccount(ctx, req.FromAccountID, req.Currency) {
+		return
+	}
+	
+	if !server.validAccount(ctx, req.ToAccountID, req.Currency) {
+		return
+	}
 
-// 	account, err := server.store.CreateAccount(ctx, arg)
-// 	if err != nil {
-// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-// 		return
-// 	}
+	arg := db.TransferTxParams{
+		FromAccountID: req.FromAccountID,
+		ToAccountID: 	 req.ToAccountID,
+		Amount: 			 req.Amount,
+	}
 
-// 	ctx.JSON(http.StatusOK, account)
-// }
+	result, err := server.store.TransferTx(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, result)
+}
+
+func (server *Server) validAccount(ctx *gin.Context, accountId int64, currency string) bool {
+	account, err := server.store.GetAccount(ctx, accountId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return false
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return false
+	}
+
+	if account.Currency != currency {
+		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", account.ID, account.Currency, currency)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return false
+	}
+
+	return true
+}
